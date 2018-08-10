f96a0f9790312dd02da6807e18bbfb58695c3cc2
asingjr2 <asingjr2@gmail.com>
2018-08-09 23:15:12 -0400

    updated settings file to make secret key an environment variable

--- xMas/settings.py
+++ xMas/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pqp97roa@3pn58r_^ua_310h$jcpf%k7nt#@b^y%k_dt6p89a='
+SECRET_KEY =  os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
